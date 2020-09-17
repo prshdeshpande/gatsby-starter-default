6319f683dfcb2e326546ba6ba9449a93d8f67ead
Paresh Deshpande <46590273+prshdeshpande@users.noreply.github.com>
2020-09-17 23:18:37 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Paresh" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Now go build something great, Paresh Deshpande.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
